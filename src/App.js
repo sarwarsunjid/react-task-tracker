bf431f462dff2ff0372ec376cdafdba6d4bcdbcb
unknown <sarwarsunjid@gmail.com>
2021-05-01 03:15:40 +0600

    Expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+// Every return has a single element
+//jsx expression must have a single element
+//basic example of jsx expression
+const name ="Brad";
+const x = false;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello this is my first react component</h1>
+      <h1>Hello from react</h1>
+      <h2>Hello {name}</h2>
+      <h2>hello {x ? "Yes" : "No"}</h2>
     </div>
   );
 }
