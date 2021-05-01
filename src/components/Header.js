372852db1841498b88a705078f803bd7e28eeaaf
unknown <sarwarsunjid@gmail.com>
2021-05-02 02:02:49 +0600

    React Events, check onClick button

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,9 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click');
+    }
     return (
         <header className = 'header'>
             {/* <h1>Hello {props.title} </h1> */}
@@ -26,10 +29,10 @@ const Header = ({title}) => {
 
             {/* compile index.css files for styling component */}
             <h1>{title}</h1>
-            <Button color ='green' text='Hello' />
+            <Button color ='green' text='Add' onClick= {onClick} />
             {/* reusable component and different props */}
-            <Button color ='red' text='Hello' />
-            <Button color ='green' text='Hello' />
+            {/* <Button color ='red' text='Hello' />
+            <Button color ='green' text='Hello' /> */}
         </header>
     )
 }
