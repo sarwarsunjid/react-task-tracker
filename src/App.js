d1fce624b4ee21d71345b1584ebd3cddc0307a22
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-03 15:43:52 +0600

    Display Optional Message, If The LIst is Empty

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ function App() {
   //moved from Task.js file for global state
   const [tasks,SetTasks] = useState([
     {
-                id:1,
-                text: 'Doctors Appoinment',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id:2,
-                text: 'Meeting at School',
-                day: 'Feb 5th at 1:30pm',
-                reminder: true,
-            },
-            {
-                id:3,
-                text: 'Food Shopping',
-                day: 'Feb 5th at 2:30pm',
-                reminder: false,
-            },
+      id:1,
+      text: 'Doctors Appoinment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'Meeting at School',
+      day: 'Feb 5th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
 ])
 
 //Delete Task
@@ -39,7 +39,8 @@ const deleteTask = (id) => {
       {/* <Header title="Programmer" />  */}
       {/* <Header title={1} /> */}
       <Header />
-      <Tasks tasks ={tasks} onDelete= {deleteTask} />
+      {tasks.length>0 ?<Tasks tasks ={tasks} 
+      onDelete= {deleteTask} />: 'No Task Added'}
     </div>
   );
 }
